bf19c16398cba2850820973418a28242160e222f
Pedro Rafael da Cruz Lança <pedro.lanca.t0124217@edu.atec.pt>
2024-03-25 12:15:26 +0000

    small updated favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
+    <link rel="icon" href="{{asset('img/logo.png')}}" type="image/x-icon">
+
     <script src="https://appsforoffice.microsoft.com/lib/1/hosted/office.js"></script>
 
     <style>
